7b3244cf6c4da07e987f7a3e4e1cc17bbb4000f4
TavGit <gustavbaim18@gmail.com>
2025-03-24 22:14:49 +0700

    upload study python for data analyst

---
+++ Latihan praktek 3.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Latihan 1
+list_angka = [1,2,3,4,5]
+arr_angka = np.array(list_angka)
+print(arr_angka)
+
+#latihan 2
+list_string= ['satu','dua','tiga']
+arr_string= np.array(list_string)
+print(arr_string)
+
+#Latihan 3
+tuple_angka = (1,2,3,4,5)
+arr_tuple = np.array(tuple_angka)
+print(arr_tuple)
+
+#Latihan 4
+arr_random = np.random.rand(2,3)
+print(arr_random)
+
+#Latihan 5
+arr_0_10 = np.arange(0,11,2)
+print(arr_0_10)
---
+++ membuat array 2x3.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+# #Membuat array 2x3 dengna nilai 0
+# arr=np.zeros((2,3))
+# print(arr)
+
+# #Membuat array 2x3 dengan nilai 1
+# arr=np.ones((7,9))
+# print(arr)
+
+# # Membuat array 2×3 dengan nilai acak
+# arr = np.empty((2, 3))
+# print(arr)
+
+
+# #Latihan 1
+# arr_zeros=np.zeros((2,3))
+# print(arr_zeros)
+
+# #Latihan 2
+# arr_ones=np.ones((2,3))
+# print(arr_ones)
+
+# #Latihan 3
+# arr_empty=np.empty((2,3))
+# print(arr_empty)
+
+# #Latihan 4
+# arr_zeros_5=np.zeros(5)
+# print(arr_zeros_5)
+
+# #Latihan 5
+# arr_ones_5=np.ones(5)
+# print(arr_ones_5)
+
+# #Latihan 6
+# arr_empty_5=np.empty(5)
+# print(arr_empty_5)
+
+# #Latihan 1
+# arr_eye_2=np.eye(2)
+# print(arr_eye_2)
+
+# #Latihan 2
+# arr_eye_3=np.eye(3)
+# print(arr_eye_3)
+
+# #Latihan 3
+# arr_eye_5=np.eye(5)
+# print(arr_eye_5)
+
+# #Latihan 4
+# arr_eye_2_4=np.eye(2,4)
+# print(arr_eye_2_4)
+
+# #Latihan 1
+# arr_rand_2_3=np.random.rand(2,3)
+# print(arr_rand_2_3)
+
+# #latihan 2
+# arr_rand_4_4=np.random.rand(4,4)
+# print(arr_rand_4_4)
+
+#latihan 1
+arr_rand_2_3=np.random.rand(2,3)
+print(arr_rand_2_3)
+
+#Latihan 2
+arr_randn_4_4=np.random.randn(4,4)
+print(arr_randn_4_4)
+
+#Latihan 3
+arr_randint_5=np.random.randint(0,10,5)
+print(arr_randint_5)
+
+#Latihan 4
+arr_randint_10=np.random.randint(5,15,10)
+print(arr_randint_10)
+
+#Latihan 5
+arr_randn_3_4=np.random.randn(3,4)
+print(arr_randn_3_4)
+
+#Latihan 6
+arr_randint_4_5=np.random.randint(0,100,(4,5))
+print(arr_randint_4_5)
